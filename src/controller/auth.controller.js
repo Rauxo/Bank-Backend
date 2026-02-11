17c77e73d8e35766edcd50b47bde22f1a712d45a
Rauxo <rahulsaikia682@gmail.com>
2026-02-11 12:07:36 +0530

    Welcome to advance banking backend project

---
+++ src/controller/auth.controller.js
@@ -0,0 +1,45 @@
+const userModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+
+async function userRegisterController(req, res) {
+  const { email, password, name } = req.bdy;
+  const isExists = await userModel.findOne({
+    email: email,
+  });
+
+  if (isExists) {
+    return res.status(422).json({
+      message: "User already exists",
+      status: "Failed",
+    });
+  }
+
+  const user = await userModel.create({
+    email,
+    password,
+    name,
+  });
+
+  const token = jwt.sign(
+    {
+      userId: user._id,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "3d" },
+  );
+
+  res.cookies("token", token);
+
+  res.status(201).json({
+    user: {
+      _id: user._id,
+      email: user.email,
+      name: user.name,
+    },
+    token,
+  });
+}
+
+module.exports = {
+  userRegisterController,
+};
---
+++ src/models/user.model.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required for crating user"],
+      trim: true,
+      lowercase: true,
+      match: [
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        "Please fill a valid email address",
+      ],
+      unique: [true, "Email already exists"],
+    },
+    name: {
+      type: String,
+      required: [true, "Name is required for creating user"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required for creating user"],
+      minlength: [6, "Password must be at least 6 characters long"],
+      select: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+userSchema.pre("save", async function (nexT) {
+  if (!this.isModified("password")) {
+    return nexT();
+  }
+
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
+
+  return nexT();
+});
+
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+const userModel = mongoose.model("user",userSchema);
+
+module.exports = userModel;
